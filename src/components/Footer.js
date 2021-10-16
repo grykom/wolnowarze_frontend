e7be67cc50ac8b39a66042ab50ffcd8a87bf66e4
grykom <grykom@gmail.com>
2021-10-16 21:59:11 +0200

    16.10

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
 function Footer(){
+    let likesStorage = JSON.parse(localStorage.getItem(`likes`));
+
+    const [liked, setLiked] = useState([])
+
+    useEffect(() => {
+        setLiked(likesStorage)
+    }, [likesStorage])
+
     return (
         <>
             <section className="page-section" id="contact">
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-8 text-center">
-                            <h2 className="mt-0">Let's Get In Touch!</h2>
+                            <h2 className="mt-0">Twoje ulubione!</h2>
                             <hr className="divider my-4" />
                             <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-                            <svg className="svg-inline--fa fa-phone fa-w-16 fa-3x mb-3 text-muted" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg>
-                            <div>+1 (555) 123-4567</div>
+                            <svg className="svg-inline--fa fa-thumbs-up fa-w-16 fa-3x mb-3 text-muted" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg>
+                            <div>{liked.map(receipe => <p>{receipe.r_id}</p>)}</div>
                         </div>
                         <div className="col-lg-4 mr-auto text-center">
                             <svg className="svg-inline--fa fa-envelope fa-w-16 fa-3x mb-3 text-muted" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg><a className="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
--- src/components/ReceipeModal.js
+++ src/components/ReceipeModal.js
@@ -22,57 +22,103 @@ Modal.setAppElement('#root');
 function ReceipeModal() {
     let history = useHistory();
     let { receipe_id } = useParams();
+    let likesStorage = JSON.parse(localStorage.getItem(`likes`));
+    if (!likesStorage) likesStorage = []
+
     const [modalIsOpen, setIsOpen] = useState(true);
     const [receipe, setReceipe] = useState();
     const [receipeReady, setReceipeReady] = useState(false);
+    const [receipeObj, setReceipeObj] = useState({})
+    const [likesNum, setLikesNum] = useState(0);
+    const [meLike, setMeLike] = useState(false);
 
     useEffect(() => {
-    fetch(API_DATA.SINGLE_RECEIPE + receipe_id)
-        .then(res => res.json())
-        .then(data => {
-            setReceipe(data);
-            setReceipeReady(true);
-        })
+        fetch(API_DATA.SINGLE_RECEIPE + receipe_id)
+            .then(res => res.json())
+            .then(data => {
+                setReceipe(data);
+                setReceipeReady(true);
+                setLikesNum(data.likes);
+                
+                const receipeObj = {
+                    r_id: data.receipe_id,
+                    r_name: data.name
+                }
+                setReceipeObj(receipeObj)          
+            })
     }, [receipe_id])
+
+    useEffect(() => {
+        likesStorage.filter(item => JSON.stringify(item) === JSON.stringify(receipeObj)).length > 0 && setMeLike(true)
+    }, [likesStorage, receipeObj])
 
     function closeModal() {
         setIsOpen(false);
         history.push('/');
     }
-  return (
-    <div>
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={customStyles}
-        >
-            <button className="btn btn-secondary float-right btn-block mb-3" onClick={closeModal}>zamknij przepis</button>
-            {receipeReady ?
-                <div className="card h-100">
-                    <Carousel autoPlay={false} showStatus={false} showIndicators={false}>
-                        {receipe.images.map((image, idx) => <img key={idx} className="img-fluid img-receipe" src={receipe.images[idx]} alt="{receipe.name}" />)}
-                    </Carousel>                    
-                    <div className="card-body pt-0">
-                        <h4 className="card-title text-primary">{receipe.name}</h4>
-                        <div className="row mb-3">
-                            <p className="card-text col-12 col-lg-6">                                
-                                Czas na HIGH: {receipe.time_on_high}<br />
-                                Czas na LOW: {receipe.time_on_low}
-                            </p>
-                            <p className="card-text col-12 col-lg-6 text-right">
-                                Liczba porcji: {receipe.serving_size}<br />
-                                Czas przygotowania: {receipe.preparing_time}                                
-                            </p>
-                        </div>
-                        <div className="row">
-                            <p className="card-text col-12 col-lg-4" dangerouslySetInnerHTML={{__html: '<h5 class="border-bottom">Składniki</h5>' + receipe.receipe_ingredients}}></p>
-                            <p className="card-text col-12 col-lg-8" dangerouslySetInnerHTML={{__html: '<h5 class="border-bottom">Sposób przygotowania</h5>' + receipe.receipe_how_to}}></p>
+    function likeIt() {
+        setLikesNum(likes => likes + 1);
+        setMeLike(true);
+        likesStorage.push(receipeObj);
+        localStorage.setItem(`likes`, JSON.stringify(likesStorage));
+        fetch(API_DATA.SINGLE_RECEIPE + receipe_id + "?likes_counter=up");
+    }
+    function dislikeIt() {
+        setLikesNum(likes => likes - 1);
+        setMeLike(false);
+        likesStorage = likesStorage.filter(item => JSON.stringify(item) !== JSON.stringify(receipeObj));
+        localStorage.setItem(`likes`, JSON.stringify(likesStorage));
+        fetch(API_DATA.SINGLE_RECEIPE + receipe_id + "?likes_counter=down");
+    }
+
+    return (
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+            >
+                <button className="btn btn-secondary float-right btn-block mb-3" onClick={closeModal}>zamknij przepis</button>
+                {receipeReady ?
+                    <div className="card h-100">
+                        <Carousel autoPlay={false} showStatus={false} showIndicators={false}>
+                            {receipe.images.map((image, idx) => <img key={idx} className="img-fluid img-receipe" src={receipe.images[idx]} alt="{receipe.name}" />)}
+                        </Carousel>                    
+                        <div className="card-body pt-0">
+                            <h4 className="col card-title text-primary">{receipe.name}</h4>                            
+                                    
+                            <div className="row bordered">
+                                <p className="card-text col-12 col-lg-6">                                
+                                    <small>Odsłon: {receipe.views}</small>                                
+                                </p>
+                                <p className="card-text col-12 col-lg-6 text-right">                                
+                                    <small>Polubień: {likesNum}</small>
+                                </p>
+                            </div>
+                            <div className="row mb-3">
+                                <p className="card-text col-12 col-lg-6">                                
+                                    <i className='fas text-primary fa-clock mr-1'></i> Czas na HIGH: {receipe.time_on_high}<br />
+                                    <i className='fas text-primary fa-clock mr-1'></i> Czas na LOW: {receipe.time_on_low}
+                                </p>
+                                <p className="card-text col-12 col-lg-6 text-right">
+                                    Liczba porcji: {receipe.serving_size}<br />
+                                    Czas przygotowania: {receipe.preparing_time}                                
+                                </p>
+                            </div>                        
+                            <div className="row">
+                                <p className="card-text col-12 col-lg-4" dangerouslySetInnerHTML={{__html: '<h5 class="border-bottom">Składniki</h5>' + receipe.receipe_ingredients}}></p>
+                                <p className="card-text col-12 col-lg-8" dangerouslySetInnerHTML={{__html: '<h5 class="border-bottom">Sposób przygotowania</h5>' + receipe.receipe_how_to}}></p>
+                            </div>
+                            <div className="text-right mt-2">
+                                <button className={`btn btn-sm btn-default ${meLike && "btn-active"}`} onClick={meLike?dislikeIt:likeIt}>Lubię to! <i className='fas text-primary fa-thumbs-up ml-2'></i></button>
+                                <button className="btn btn-sm btn-default">Drukuj <i className='fas text-primary fa-print ml-2'></i></button>
+                                <button className="btn btn-sm btn-default">Dodaj do ulubionych <i className='fas text-primary fa-list ml-2'></i></button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            : <h3>Ładowanie przepisu...</h3>}
-        </Modal>
-    </div>
+                : <h3>Ładowanie przepisu...</h3>}
+            </Modal>
+        </div>
   );
 }
 
